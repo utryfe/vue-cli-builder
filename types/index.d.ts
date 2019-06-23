dc613023e17b41d398ad36571dbd142c037ac783
star2018 <star2018@outlook.com>
2019-06-23 11:37:22 +0800

    chore: support build_code_splitting_exclude config prop.

--- types/index.d.ts
+++ types/index.d.ts
@@ -19,6 +19,7 @@ export interface PreprocessConfig {
   routerViews?: string
   kebabCasePath?: boolean
   codeSplitting?: boolean
+  codeSplittingExclude?: string
   appPlugins?: string
   appUseVuex?: boolean
   appUseRouter?: boolean
